62ba305ae84190a9686b3b6d9f2827ee1b94a10c
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-06-23 00:31:55 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+#Create a dog class
+#Add three variables to the class. fido, snoopy and lassie
+class Dog
+end
+
+fido = Dog.new
+fido
+snoopy = Dog.new
+snoopy
+lassie = Dog.new
+lassie
+
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+#Add a class definition for a person class
+
+class Person 
+end
+
+adele_goldberg = Person.new
+adele_goldberg
+alan_kay = Person.new
+alan_kay
